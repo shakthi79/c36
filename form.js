2a1bc9c585f2cc6340479089eff28cdd007f48b3
shakthi79 <69965461+shakthi79@users.noreply.github.com>
2021-05-25 16:36:39 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,36 @@
+class Form {
+    constructor (){
+        this.title = createElement('h2')
+        this.input = createInput('name')
+        this.button = createButton('play')
+        this.greeting = createElement('h3')
+    }
+    hide(){
+        this.input.hide()
+        this.button.hide()
+        this.greeting.hide()
+    }
+    display(){
+        
+        this.title.html('car racing game')
+        this.title.position(130,0)
+        
+        this.input.position(130,160)
+        
+
+        this.button.position(250,200)
+        this.button.mousePressed(()=>{
+            this.input.hide()
+            this.button.hide()
+            player.name = this.input.value()
+            playerCount+=1
+            player.index = playerCount
+            player.update()
+            player.updateCount(playerCount)
+            
+            this.greeting.html('hello'+ player.name)
+            this.greeting.position(130,160)
+        })
+    
+    }
+}
